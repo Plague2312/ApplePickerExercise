b3445202680c81295262044f19ee1aba0515f4af
Ethan Landrum <landrum.ethan.2312@gmail.com>
2022-01-31 16:41:21 -0600

    ApplePicker First Update

---
+++ ApplePicker/Assets/Scripts/Apple.cs
@@ -0,0 +1,27 @@
+/***
+ * Created By: Ethan Landrum
+ * Date Created: 1/31/2022
+ * 
+ * Last Edited: N/A
+ * Last Edited By: N/A
+ * 
+ * Description: Controls tree movement and apple spawning.
+ * 
+ * **/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
